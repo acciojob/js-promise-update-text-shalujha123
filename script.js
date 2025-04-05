a850db513f49ee8120ff79af11a8a79b7fb18a50
Shalu <88194182+shalujha123@users.noreply.github.com>
2025-04-05 18:10:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 let promise = new Promise((resolve,reject) => {
 	setTimeout(() => {
-		resolve("Hello world!")
+		resolve("Hello, world!")
 	},1000)
 })
 .then((msg) => {
